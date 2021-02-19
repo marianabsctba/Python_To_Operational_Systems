45ff5f058656a741db65fbf5e998dcf00b13ebff
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-02-19 16:49:43 -0300

    Update Questão_06_TP1.py

--- Questão_06_TP1.py
+++ Questão_06_TP1.py
@@ -2,9 +2,27 @@
 
 import os.path
 
+# versão A (arquivo predefinido)
+
+def line():
+    print("==" * 40)
 
 def file_extension():
+    line()
+    print("Versão A do exercício")
     return os.path.splitext('q1.py')[1]
 
+# versão B (usando como base os arquivos encontrados no diretório corrente)
 
-print("A extensão do arquivo é: ", file_extension())
+def file_extension_two():
+    line()
+    print("Versão B do exercício")
+    print("As extensões encontradas para o diretório corrente são: ")
+    for file_name in os.listdir('.'):
+        name, extension = os.path.splitext(file_name)
+        print(extension)
+
+
+print('A extensão do arquivo é: ', file_extension())
+file_extension_two()
+
