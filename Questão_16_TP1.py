2c6a05620bc6377da16c36b7e4fd80852a7260d7
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-02-20 17:59:07 -0300

    Update Questão_16_TP1.py

--- Questão_16_TP1.py
+++ Questão_16_TP1.py
@@ -1,19 +1,24 @@
 #QUESTÃO 16 - Escreva um programa em Python, usando o módulo ‘psutil’, que imprima o tempo de CPU em segundos por núcleo.
 
+
 import psutil, time
 
+def line():
+    print("==" * 40)
 
-def core_times(core):   #versão A
-    num_cpu = len(core)
-    for i in core:
-        print(f"Tempo de cada núcleo: ", i)
-        
+def core_times():   #versão A #impressão organizada de todos os valores
+    line()
+    print("CPU - tempos por core por segundo (versão A):")
+    for i, time in enumerate(psutil.cpu_times(percpu=True)):
+        print(f"Core {i+1} -  User: {time.user:.2f} - Sistema:{time.system:.2f} - Idle: {time.idle:.2f} - Interrupt: {time.interrupt:.2f} - DPC: {time.dpc:.2f}")
 
-def core_times2(): # versão B
+def core_times2(): # versão B #impressão dos valores "crus"
+    line()
+    print("(Versão B)")
     cpu_info = psutil.cpu_times(percpu=True)
     for i in cpu_info:
-        print("Tempos por núcleo", i)
+        print("Tempos por núcleo: ", i)
  
     
-core_times(psutil.cpu_times(percpu=True))
+core_times()
 core_times2()
