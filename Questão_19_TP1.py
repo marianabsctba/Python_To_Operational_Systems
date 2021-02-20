1183e4a8bc903a69602c3949ed3bbdc94502ac7b
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-02-20 17:11:18 -0300

    Update Questão_19_TP1.py

--- Questão_19_TP1.py
+++ Questão_19_TP1.py
@@ -1,6 +1,7 @@
 #QUESTÃO 19 -Escreva um programa em Python, usando o módulo ‘psutil’, que imprima em GB,
 #quanto de armazenamento disponível há na partição do sistema (onde o sistema está instalado).
 
+
 import psutil
 
 
