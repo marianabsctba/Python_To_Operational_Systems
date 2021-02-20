8f8d8fc5c9e39d9e6e267d2b745fb3531a06c2e8
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-02-20 19:26:08 -0300

    Update Questão_15_TP1.py

--- Questão_15_TP1.py
+++ Questão_15_TP1.py
@@ -3,21 +3,27 @@
 
 import psutil, subprocess, time
 
-
-c = subprocess.Popen("calc")
+c = subprocess.Popen("calc") #apenas para exemplificar, peguei o PID da calculadora
 p = c.pid
+
+def size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
 
 def info_pid(p):
     info = psutil.Process(p)
     return info
 
 def print_information(p):
+    mem = psutil.virtual_memory()
     print("Nome do usuário: ", information.username())
     print("Tempo de criação: ", time.ctime(information.create_time()))
-    print("Uso de memória(KB): ", information.memory_info().rss/1024)
+    print("Uso de memória: ", size(information.memory_info().rss/1024))
 
 
 print("PID: ", p)
 information = info_pid(p)
 print_information(p)
-    
